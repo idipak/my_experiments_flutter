3f812e218deba67200e717626d0efed83dd5112b
idipak <dipak_pd@outlook.com>
2022-10-29 20:36:44 +0530

    hero example

--- lib/animations/hero_example.dart
+++ lib/animations/hero_example.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:my_experiments/animations/cross_fade_animation.dart';
 import 'package:my_experiments/animations/list_animation.dart';
 
-class HeroExample extends StatelessWidget {
+class HeroExample extends StatefulWidget {
   const HeroExample({Key? key}) : super(key: key);
+
+  @override
+  State<HeroExample> createState() => _HeroExampleState();
+}
+
+class _HeroExampleState extends State<HeroExample> {
+  bool _isFirst = true;
+  bool _logoTaped = false;
+
+  _tapLogo(){
+    setState(() {
+      _logoTaped = !_logoTaped;
+    });
+  }
+
+  _change(){
+    setState(() {
+      _isFirst = !_isFirst;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,26 +37,51 @@ class HeroExample extends StatelessWidget {
 
           TextButton(onPressed: (){
             Navigator.push(context, MaterialPageRoute(builder: (_) => const CrossFadeAnimation()));
-          }, child: const Text("CrossFade"))
+          }, child: const Text("CrossFade", style: TextStyle(color: Colors.black),))
         ],
       ),
       body: Column(
         children: [
-          Row(children: [],),
+          Row(children: const [],),
 
-          const Hero(
-            tag: "ex",
-            child: FlutterLogo(size: 200,),
+          AnimatedAlign(
+              alignment: _isFirst ? Alignment.topLeft : Alignment.topRight,
+              duration: const Duration(seconds: 1),
+            child: const FlutterLogo(size: 50,),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: _tapLogo,
+              child: AnimatedPhysicalModel(
+                duration: const Duration(milliseconds: 500),
+                color: Colors.white,
+                shadowColor: Colors.black,
+                elevation: !_logoTaped ? 0.0 : 6.0,
+                shape: BoxShape.rectangle,
+                child: const Hero(
+                  tag: "ex",
+                  child: FlutterLogo(size: 200,),
+                ),
+              ),
+            ),
           ),
 
           AnimatedDefaultTextStyle(
-              child: const Text("Animated Text"),
-              style: const TextStyle(fontSize: 32, color: Colors.cyan),
-              duration: Duration(milliseconds: 500)),
+            curve: Curves.easeOutQuint,
+              style: _isFirst ? const TextStyle(fontSize: 16, color: Colors.blue) : const TextStyle(fontSize: 32, color: Colors.cyan),
+              duration: const Duration(milliseconds: 1000),
+            onEnd: (){
+              _isFirst = true;
+              setState(() {
 
-          MaterialButton(onPressed: (){
+              });
+            },
+            child: const Text("Animated Text"),
+          ),
 
-          }, child: const Text("Change"),)
+          MaterialButton(onPressed: _change, child: const Text("Change"),)
         ],
       ),
     );
