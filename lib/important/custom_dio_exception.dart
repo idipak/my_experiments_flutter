1e1cbc2e68b6156b378db078738b106576c7d789
Dipak <dipak_pd@outlook.com>
2022-12-08 11:08:09 +0530

    field validation and dio exception example

---
+++ lib/important/custom_dio_exception.dart
@@ -0,0 +1,52 @@
+//
+// import 'package:dio/dio.dart';
+//
+// class CustomException implements Exception {
+//   CustomException.fromDioError(DioError dioError) {
+//     switch (dioError.type) {
+//       case DioErrorType.cancel:
+//         message = "Request to API server was cancelled";
+//         break;
+//       case DioErrorType.connectTimeout:
+//         message = "Connection timeout with API server";
+//         break;
+//       case DioErrorType.other:
+//         message = "Connection to API server failed due to internet connection";
+//         break;
+//       case DioErrorType.receiveTimeout:
+//         message = "Receive timeout in connection with API server";
+//         break;
+//       case DioErrorType.response:
+//         message = _handleError(dioError.response!.statusCode, dioError.response?.data);
+//         break;
+//       case DioErrorType.sendTimeout:
+//         message = "Send timeout in connection with API server";
+//         break;
+//       default:
+//         message = "Something went wrong";
+//         break;
+//     }
+//   }
+//
+//   String? message;
+//
+//   String _handleError(statusCode, response) {
+//     switch (statusCode) {
+//       case 400:
+//         return 'Bad request';
+//       case 404:
+//         return 'The requested resource was not found';
+//       case 500:
+//         return 'Internal server error';
+//       default:
+//       // final msg = response != null
+//       //     ? response['message']
+//       //     : "Oops something went wrong";
+//       // return msg;
+//         return 'Oops something went wrong';
+//     }
+//   }
+//
+//   @override
+//   String toString() => message.toString();
+// }
